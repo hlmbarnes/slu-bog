7df5a30299690dd5e2472d8a39f8bfe365cf0853
Hannah Barnes <hlmbarnes@me.com>
2016-05-04 23:59:30 -0700

    updated navigation bar and worked on routing for creatures

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,11 +1,11 @@
 class CreaturesController < ApplicationController
   def index
-    @creature =Creature.all{ |e| }
+    @creatures = Creature.all
   end
 
   def create
-    Tweet.create creature_params
-    redirect_to creature_path
+    creature = Creature.create creature_params
+    redirect_to creature_path creature
   end
 
   def new
@@ -13,21 +13,27 @@ class CreaturesController < ApplicationController
   end
 
   def edit
+    @creature = Creature.find params[:id]
   end
 
   def show
-
+    @creature = Creature.find params[:id]
   end
 
   def update
+    c = Creature.find params[:id]
+    c.update creature_params
+    redirect_to creatures_path
   end
 
   def destroy
+    Creature.find(params[:id]).delete
+    redirect_to creatures_path
   end
 
   private
+
   def creature_params
     params.require(:creature).permit(:name, :description)
   end
-
 end
